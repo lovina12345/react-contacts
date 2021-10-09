275aff3d28b3403dc137dc2dab5be60a998d8be9
lovina <lovinaafrifa223344@gmail.com>
2021-10-09 04:01:54 -0700

    first commit

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,20 @@
+var initialState = {
+    users: [
+        {
+            name:"lovina",
+        gen:"23"
+    },
+
+    {
+        name:"lovina",
+    gen:"23"
+},
+
+    ]
+}
+
+ var contactReducer=(state=initialState,action)=>{
+return state
+}
+
+export default contactReducer;
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import {createStore} from "redux"
+import contactReducer from "../reducers/contactReducer"
+
+var store = createStore(contactReducer)
+
+
+
+export default store;
