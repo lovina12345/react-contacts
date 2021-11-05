f81ee7a59234df8b965812046268f0d5665fd3df
lovina <lovinaafrifa223344@gmail.com>
2021-11-05 15:19:49 -0700

    1st commit

---
+++ src/firebase/Config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAYlKi7eFqcOwjMAlKQx7onzCOijkjc0B4",
+    authDomain: "react-contacts-93d9e.firebaseapp.com",
+    projectId: "react-contacts-93d9e",
+    storageBucket: "react-contacts-93d9e.appspot.com",
+    messagingSenderId: "33745937719",
+    appId: "1:33745937719:web:331f4b849ed0359ae026b2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default firebase;
